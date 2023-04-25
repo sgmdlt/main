251d8abf0583e5c5c27b32cc779dd7f91590e5fb
Ivan Mamtsev <ivan.mamtsev@hexlet.io>
2023-04-25 20:16:51 +0400

    remove procfile

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,7 @@ logging.basicConfig(
     style='{',
 )
 
-SECRET_KEY = misc.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', 'true').lower() in {'yes', '1', 'true'}
 
